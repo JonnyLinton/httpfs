1b7cc65f95eda8ef181225901ddbe0b3445a1e6d
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-11 17:37:52 -0500

    Added Function Names

---
+++ file_handling.py
@@ -0,0 +1,11 @@
+def get_file(pathname):
+    # Jonny
+    # returns either directory tree or contents of file
+
+def post_file(pathname, file_content, overwrite=true):
+    # Louis-Olivier
+    # returns true or false
+
+def isvalid(pathname):
+    # Louis-Olivier
+    # returns true if access to directory is allowed
---
+++ httpfs_helper_functions.py
@@ -0,0 +1,14 @@
+import socket
+
+def handle_get(request_headers):
+    # Jonny
+    # returns GET response headers and body
+
+def handle_post(request_headers_and_data):
+    # Louis-Olivier
+    # returns POST response headers and body
+
+def header_parsing(header):
+    # Louis-Olivier
+    # takes header and stores every element in Dictionary eg: ["Content-Type":"text/html"]
+    # returns Dictionary
