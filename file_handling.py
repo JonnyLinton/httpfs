36778c99958fa2d6d032168d72adba5a8370b89d
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-11 23:58:02 -0500

    Changed list_files so that it returns, not prints

--- file_handling.py
+++ file_handling.py
@@ -3,7 +3,7 @@ from HTTPException import HTTPException
 
 SERVER_DIRECTORY = os.getcwd() + "/files"
 
-# returns either directory tree or contents of file
+# Perform GET on pathname
 def get_file(pathname):
     # Check if user has access to the file
     user_has_access(pathname)
@@ -18,6 +18,7 @@ def get_file(pathname):
         with open(SERVER_DIRECTORY+pathname, 'r') as content_file:
             return content_file.read()
 
+# Perform POST on inputted file
 def post_file(pathname, file_content, overwrite=True):
     # Check if user access is granted
     user_has_access(pathname)
@@ -36,25 +37,29 @@ def post_file(pathname, file_content, overwrite=True):
     file.closed
     return True
 
+# Returns true if user has access, raises HTTPException(403) if access denied
 def user_has_access(pathname):
-    # returns true if user has access, raises HTTPException(403) if access denied
     return True
 
+# Returns true if pathname exists, raises HTTPException(404) if it does not
 def pathname_exists(pathname):
-    # returns true if pathname exists, raises HTTPException(404) if it does not
     return True
 
+# Returns true is file with pathname exists, raises HTTPException(404) if not
 def file_exists(pathname):
     if os.path.isfile(pathname):
         return True
     else:
         raise HTTPException(404)
 
+# Prints complete directory tree for path
 def list_files(startpath):
+    tree = ""
     for root, dirs, files in os.walk(startpath):
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
+        tree += '{}{}/'.format(indent, os.path.basename(root)) + "\n"
         subindent = ' ' * 4 * (level + 1)
         for f in files:
-            print('{}{}'.format(subindent, f))
+            tree += '{}{}'.format(subindent, f) + "\n"
+    return tree
