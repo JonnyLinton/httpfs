441bc34f993d60bd6a87634d8851eac40152052c
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-11 23:15:31 -0500

    Implemented header_parsing()

--- httpfs_helper_functions.py
+++ httpfs_helper_functions.py
@@ -1,14 +1,20 @@
 import socket
-
+import re
 def handle_get(request_headers):
     # Jonny
     # returns GET response headers and body
+    return True
 
 def handle_post(request_headers_and_data):
     # Louis-Olivier
     # returns POST response headers and body
+    return True
 
 def header_parsing(header):
-    # Louis-Olivier
-    # takes header and stores every element in Dictionary eg: ["Content-Type":"text/html"]
-    # returns Dictionary
+    dict = {}
+    lines =  re.split("\n+", header)
+    requests_list = [re.split(": ", entry, 2) for entry in lines]
+    for elements in requests_list:
+        if len(elements) > 1:
+            dict[elements[0]] = elements[1]
+    return dict
