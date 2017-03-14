5d58bbacb8af5f971c49fa11c3c293deac0671ad
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-14 14:32:06 -0400

    Added parsing functions

--- httpfs_helper_functions.py
+++ httpfs_helper_functions.py
@@ -17,8 +17,40 @@ Content-Type: text/html
 
 data data data data data'''
 
+sampleRequestGET = '''GET / HTTP/1.1
+Host: louisolivier.com
+Connection: keep-alive
+Pragma: no-cache
+Cache-Control: no-cache
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Encoding: gzip, deflate, sdch
+Accept-Language: en,en-US;q=0.8,fr-CA;q=0.6,fr;q=0.4
+'''
+
+sampleRequestPOST = '''POST /path/script.cgi HTTP/1.0
+From: frog@jmarshall.com
+User-Agent: HTTPTool/1.0
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 32
+
+home=Cosby&favorite+flavor=flies'''
+
 def receive_request(data):
     # louis
+    request_type = request_type_parsing(data)
+    header_dictionary = header_parsing(data)
+    pathname = pathname_parsing(data)
+    body = body_parsing(data)
+    if request_type == "GET":
+        handle_get(header_dictionary, pathname)
+    elif request_type == "POST":
+        handle_post(header_dictionary, body, pathname)
+    else:
+        raise HTTPException(400)
+
+
 
 def getDate():
     now = datetime.now()
@@ -52,3 +84,18 @@ def header_parsing(header):
         if len(elements) > 1:
             dict[elements[0]] = elements[1]
     return dict
+
+def request_type_parsing(header):
+    return re.search(r"^[^ /]*", header).group(0)
+
+def pathname_parsing(header):
+    return re.search(r"\s(.*?)\s", header).group(0)
+
+def body_parsing(request):
+    data_content = request.split("\r\n\r\n")
+    if len(data_content) > 1:
+        return data.split("\r\n\r\n")[1]
+    else:
+        return ""
+
+print(body_parsing(sampleRequestPOST))
