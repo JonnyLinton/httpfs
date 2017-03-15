c0634db84dfcd598deaa0338bfdeb50a6d334552
JonnyLinton <linton.jonny@gmail.com>
2017-03-14 21:23:53 -0400

    adding HTTPException handling into response codes

--- httpfs_server.py
+++ httpfs_server.py
@@ -3,6 +3,7 @@ import threading
 import argparse
 from httpfs_helper_functions import handle_get
 from httpfs_helper_functions import receive_request
+from HTTPException import *
 
 
 def run_server(verbose, server_working_directory, port=8080):
@@ -30,10 +31,28 @@ def handle_client(conn, addr, verbose, server_working_directory):
         #     if not data:
         #         break
         print("Receiving request: \n" +data.decode("utf-8"))
-        response = receive_request(data.decode("utf-8"), verbose, str(server_working_directory))
-        response = response.encode("utf-8")
-        # split content into smaller pieces?
-        print("\nSending response: \n" +response.decode("utf-8"))
-        conn.sendall(response)
+        response = "".encode("utf-8")
+        try:
+            response = receive_request(data.decode("utf-8"), verbose, str(server_working_directory))
+            response = response.encode("utf-8")
+        except HTTPException as e:
+            # catch any HTTPExceptions and instead transform it into the appropriate response code
+            http_code = e.args[0]
+            http_code_name = http_name_from_code(http_code)
+            response = "HTTP/1.1 " +str(http_code) +" " +http_code_name +"\r\n\r\n"
+            response = response.encode("utf-8")
+        finally:
+            print("\nSending response: \n" +response.decode("utf-8"))
+            conn.sendall(response)
     finally:
         conn.close()
+
+def http_name_from_code(http_code):
+    if(http_code == 400):
+        return "Bad Request"
+    elif(http_code == 403):
+        return "Forbidden"
+    elif(http_code == 404):
+        return "Not Found"
+    else:
+        raise Exception("Louis and Jonny have not finished their code correctly")
