bdf5176242557c69f7af11e712523a24e966edf1
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-16 00:56:53 -0400

    Removed debugging prints, added logging

--- httpfs_server.py
+++ httpfs_server.py
@@ -6,7 +6,7 @@ from httpfs_helper_functions import receive_request
 from HTTPException import *
 from logger_init import logger
 
-def run_server(verbose, server_working_directory, port=8080):
+def run_server(verbose, server_working_directory, port=8081):
     # Disable the logger if verbose is False
     logger.disabled = not verbose
     logger.warning("Server initialized at port %s", port)
@@ -16,7 +16,7 @@ def run_server(verbose, server_working_directory, port=8080):
     try:
         listener.bind((host, port))
         listener.listen(5)
-        print('http file server is listening at', port)
+        print('HTTPfs is listening at', port)
         while True:
             conn, addr = listener.accept()
             threading.Thread(target=handle_client, args=(conn, addr, verbose, server_working_directory)).start()
@@ -25,15 +25,8 @@ def run_server(verbose, server_working_directory, port=8080):
 
 def handle_client(conn, addr, verbose, server_working_directory):
     logger.info('New connection: %s', str(addr))
-    print ('New client from', addr)
     try:
         data = conn.recv(1024)
-        # while True:
-        #     print("in loop")
-        #     data += conn.recv(256)
-        #     print("after recv")
-        #     if not data:
-        #         break
         print("Receiving request: \n" +data.decode("utf-8"))
         response = "".encode("utf-8")
         try:
@@ -43,7 +36,7 @@ def handle_client(conn, addr, verbose, server_working_directory):
             # catch any HTTPExceptions and instead transform it into the appropriate response code
             http_code = e.args[0]
             http_code_name = http_name_from_code(http_code)
-            response = "HTTP/1.1 " +str(http_code) +" " +http_code_name +"\r\n\r\nError " +str(http_code) +" (" +http_code_name +") !!" 
+            response = "HTTP/1.1 " +str(http_code) +" " +http_code_name +"\r\n\r\nError " +str(http_code) +" (" +http_code_name +") \n"
             response = response.encode("utf-8")
         finally:
             print("\nSending response: \n" +response.decode("utf-8"))
@@ -54,10 +47,13 @@ def handle_client(conn, addr, verbose, server_working_directory):
 
 def http_name_from_code(http_code):
     if(http_code == 400):
+        logger.error("Bad Request %i", http_code)
         return "Bad Request"
     elif(http_code == 403):
+        logger.error("Forbidden %i", http_code)
         return "Forbidden"
     elif(http_code == 404):
+        logger.error("Not Found %i", http_code)
         return "Not Found"
     else:
-        raise Exception("Louis and Jonny have not finished their code correctly")
+        raise Exception("Something went wrong. Please submit your request again.")
