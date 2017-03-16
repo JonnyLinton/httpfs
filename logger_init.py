4719a0caa1246b7f434438927fb16fdf82e6d8c9
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-15 21:14:39 -0400

    Added logger output to console

--- logger_init.py
+++ logger_init.py
@@ -3,8 +3,14 @@ import os
 
 logger = logging.getLogger("httpfs_logger")
 logger.setLevel(logging.DEBUG)
+# Send output of logger to access.log
 file_handler = logging.FileHandler(os.getcwd() + "/access.log")
+# Also send output to console
+stream_handler = logging.StreamHandler()
+
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s  - %(message)s')
 file_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 logger.disabled = False
