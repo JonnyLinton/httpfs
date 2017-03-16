128f72256a6fa2cb24790ac2b0a4e5248b16544f
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-03-16 00:57:06 -0400

    Removed streamHandling

--- logger_init.py
+++ logger_init.py
@@ -6,11 +6,11 @@ logger.setLevel(logging.DEBUG)
 # Send output of logger to access.log
 file_handler = logging.FileHandler(os.getcwd() + "/access.log")
 # Also send output to console
-stream_handler = logging.StreamHandler()
+# stream_handler = logging.StreamHandler()
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s  - %(message)s')
 file_handler.setFormatter(formatter)
-stream_handler.setFormatter(formatter)
+# stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+# logger.addHandler(stream_handler)
 logger.disabled = False
